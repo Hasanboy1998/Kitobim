d087ed02a7c785292cd639abffd6e91667c52e93
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2023-01-18 16:42:21 +0500

    -add response

---
+++ app/src/main/java/uz/gita/khasanboy_kitobim/data/responses/GetAllDataResponse.kt
@@ -0,0 +1,5 @@
+package uz.gita.khasanboy_kitobim.data.responses
+
+sealed interface GetAllDataResponse{
+
+}
--- app/src/main/java/uz/gita/khasanboy_kitobim/domain/repositories/impl/FireStoreRepositoryImpl.kt
+++ app/src/main/java/uz/gita/khasanboy_kitobim/domain/repositories/impl/FireStoreRepositoryImpl.kt
@@ -1,18 +1,36 @@
 package uz.gita.khasanboy_kitobim.domain.repositories.impl
 
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import uz.gita.khasanboy_kitobim.data.UploadData
+import uz.gita.khasanboy_kitobim.data.common.BookData
+import uz.gita.khasanboy_kitobim.data.mapper.BooksMapper.toBookData
 import uz.gita.khasanboy_kitobim.domain.repositories.FireStoreRepository
 import javax.inject.Inject
 
-class FireStoreRepositoryImpl @Inject constructor(private val storage: FirebaseStorage, private val database: CollectionReference) :
+class FireStoreRepositoryImpl @Inject constructor(
+    private val storage: FirebaseStorage,
+    private val database: CollectionReference
+) :
     FireStoreRepository {
 
-    init {
+    private var snapshotListener: ListenerRegistration? = null
 
+    init {
+        snapshotListener = database
+            .addSnapshotListener { value, error ->
+                val list = ArrayList<BookData>()
+                value?.forEach { document ->
+                    list.add(
+                        document.toBookData()
+                    )
+                }
+                dataFlow.tryEmit(list)
+                value?.let { errorFlow.tryEmit("it") }
+            }
     }
 
     override fun downloadFile(bookNameStorage: String): Flow<Result<UploadData>> = callbackFlow {
